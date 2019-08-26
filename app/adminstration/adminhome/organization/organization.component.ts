f27f3f6f257a085877afdb056d15b639a1c45716
mopursreekanth <37208851+mopursreekanth@users.noreply.github.com>
2019-08-26 12:47:28 +0530

    Add files via upload

---
+++ app/adminstration/adminhome/organization/organization.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization',
+  templateUrl: './organization.component.html',
+  styleUrls: ['./organization.component.css']
+})
+export class OrganizationComponent implements OnInit {
+
+
+  title = '';
+
+  columnDefs = [
+      {headerName: 'Make', field: 'make' },
+      {headerName: 'Model', field: 'model' },
+      {headerName: 'Price', field: 'price'}
+  ];
+
+  rowData = [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
